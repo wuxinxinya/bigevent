9f0b1384ca7f8f8015785fccdb96341aabe4ef72
wuxinxinya <1252857980@qq.com>
2020-12-18 17:27:48 +0800

    用户资料更新

---
+++ assets/js/userinfo.js
@@ -0,0 +1,31 @@
+var form = layui.form;
+// 获取信息反填到页面    表单---表单取值/赋值
+function render() {
+  $.ajax({
+    url: '/my/user/userinfo',
+    success: function (res) {
+      // console.log(res);
+      if (res.status === 0) {
+        form.val('user', res.data)
+      }
+    }
+  })
+}
+render()
+// 更新信息
+$('.layui-card form').on('submit', function (e) {
+  e.preventDefault()
+  var data = $(this).serialize()
+  $.ajax({
+    type: 'post',
+    data: data,
+    url: '/my/user/userinfo',
+    success: function (res) {
+      // console.log(res);
+      layer.msg(res.message)
+      if (res.status === 0) {
+        window.parent.getUserInfo()
+      }
+    }
+  })
+})
