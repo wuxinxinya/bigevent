9bf39a93c29b2066342eeb8fec5bfa48e7dc933c
wuxinxinya <1252857980@qq.com>
2020-12-21 18:03:58 +0800

    文章管理---文章列表--列表渲染时间处理---利用template过滤器

--- assets/js/article/list.js
+++ assets/js/article/list.js
@@ -1,3 +1,26 @@
+// ----tempalte过滤器，时间处理-------
+function padZero(n) {
+  if (n < 10) {
+    return '0' + n
+  } else {
+    return n
+  }
+}
+
+template.defaults.imports.dateFormat = function (dtStr) {
+  var dt = new Date(dtStr)
+
+  var y = dt.getFullYear()
+  var m = padZero(dt.getMonth() + 1)
+  var d = padZero(dt.getDate())
+
+  var hh = padZero(dt.getHours())
+  var mm = padZero(dt.getMinutes())
+  var ss = padZero(dt.getSeconds())
+
+  return y + '-' + m + '-' + d + ' ' + hh + ':' + mm + ':' + ss
+}
+
 // ----------------获取数据并渲染---------------
 var data = {
   pagenum: 1,//当前页
